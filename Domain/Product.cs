1a7100189e017e755363f5f157dfeb365d36d367
Adeilton Rodrigues <adeiltonjr15@gmail.com>
2025-05-26 20:47:24 -0300

    adiciona camada de dominio com entidade de produto

---
+++ Domain/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain;
+
+public class Product : Entity
+{
+    public required string Nome { get; set; }
+    public decimal Price { get; set; }
+    public required string Category { get; set; }
+    public required string ImageUri { get; set; }
+}
