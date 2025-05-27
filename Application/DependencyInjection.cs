24448d71141209bb8e401eb34d2f90410246bbb4
Adeilton Rodrigues <adeiltonjr15@gmail.com>
2025-05-26 22:08:07 -0300

    adiciona camada de aplicação com lógica de negócio de criação de produto

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Application.UseCases;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDependencies(IServiceCollection services)
+    {
+        services.AddMediator(opts =>
+        {
+            opts.ServiceLifetime = ServiceLifetime.Scoped;
+        });
+
+        services.AddValidatorsFromAssemblyContaining<CreateProductRequest.Validator>();
+
+        return services;
+    }
+}
---
+++ Application/UseCases/CreateProductRequest.cs
@@ -0,0 +1,84 @@
+﻿using Domain.Images;
+using Domain.Products;
+using FluentResults;
+using FluentValidation;
+using Mediator;
+
+namespace Application.UseCases;
+
+public record CreateProductRequest(
+    string Name,
+    int CategoryId,
+    decimal Price,
+    IEnumerable<Image> Images
+) : IRequest<Result<CreateProductRequest.Response>>
+{
+    public class Validator : AbstractValidator<CreateProductRequest>
+    {
+        public Validator()
+        {
+            RuleFor(product => product.Price)
+                .GreaterThanOrEqualTo(0);
+
+            RuleFor(product => product.CategoryId)
+                .GreaterThan(0);
+
+            RuleForEach(product => product.Images)
+                .ChildRules(image => {
+                    image.RuleFor(i => i.Extension).NotEmpty();
+                    image.RuleFor(i => i.Name).NotEmpty();
+                    image.RuleFor(i => i.Stream); // TODO: Adiciona validação de tipo por assinatura do binario
+                });
+
+            RuleFor(product => product.Name)
+                .NotEmpty();
+        }
+    }
+
+    public record Response(int Id);
+
+    public class Handler : IRequestHandler<CreateProductRequest, Result<Response>>
+    {
+        private readonly Product.IRepository productRepository;
+        private readonly Image.IRepository imageRepository;
+        private readonly Product.Categories.IRepostiory categoriesRepository;
+
+        public Handler(
+            Product.IRepository productRepository,
+            Image.IRepository imageRepository,
+            Product.Categories.IRepostiory categoriesRepository
+        )
+        {
+            this.productRepository = productRepository;
+            this.imageRepository = imageRepository;
+            this.categoriesRepository = categoriesRepository;
+        }
+
+        public async ValueTask<Result<Response>> Handle(CreateProductRequest request, CancellationToken cancellationToken)
+        {
+            var category = await categoriesRepository.Get(request.CategoryId, cancellationToken);
+
+            if (category is null) return new ResourceNotFoundError("Categoria");
+
+            var imagesUrisByImageName = await imageRepository.Create(request.Images, cancellationToken);
+
+            var product = new Product
+            {
+                Category = category,
+                Name = request.Name,
+                Price = request.Price,
+                Images = request.Images.Select(image => 
+                    new Product.Image
+                    {
+                        Uri = imagesUrisByImageName[image.Name]
+                    }
+                )
+            };
+
+            await productRepository.Create(product, cancellationToken);
+
+            return new Response(product.Id);
+        }
+    }
+}
+
