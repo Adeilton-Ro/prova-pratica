ed22265447abddf764e2d5282b9c14b67bf10d9d
Adeilton Rodrigues <adeiltonjr15@gmail.com>
2025-05-26 23:20:43 -0300

    adiciona campo "isActive" em produto

--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -1,11 +1,10 @@
-﻿using Domain.Images;
-
-namespace Domain.Products;
+﻿namespace Domain.Products;
 
 public partial class Product : Entity
 {
     public required string Name { get; set; }
     public decimal Price { get; set; }
+    public bool IsActive { get; set; } = true;
     public required Categories Category { get; set; }
     public IEnumerable<Image> Images { get; set; } = [];
 
